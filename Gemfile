3430e60f5f1ac00fce7cc2ca7f2c8fe5d18005d8
cbs-clicrdv <39952731+cbs-clicrdv@users.noreply.github.com>
2020-11-16 11:46:39 +0100

    git init

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
-ruby '2.5.7'
+ruby '2.3.3'
 gem 'rspec'
 gem 'byebug'
 gem 'rubocop', '~> 0.57.2'
--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
 
 ## Here goes your code !
-
+a = 'salut'
+puts a
 ## You can create an object inside './lib/' => my_object.rb 
 ## Or you can create folders inside './lib/' for specific objects !
